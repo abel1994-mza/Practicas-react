7b85076fd0e1afff84707c72b62d5d3be28e071b
Abel Contreras <lucas_65_@hotmail.es>
2025-05-07 12:16:17 +0200

    Commit en segunda parte

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,5 @@
-import React, { useState } from "react";
-
 const App = () => {
-  const frases = [
-    "La práctica hace al maestro.",
-    "Divide y vencerás.",
-    "El código limpio es una forma de arte.",
-    "Aprender React es divertido.",
-    "Nunca dejes de mejorar.",
-  ];
-
-  const [indice, setIndice] = useState(0);
-  const [cambio, setCambio] = useState(0);
-  const [like, setLike] = useState(0);
-
-  const siguienteFrase = () => {
-    const nuevo = Math.floor(Math.random() * frases.length);
-    setCambio(nuevo);
-    setIndice(nuevo + 1);
-  };
-  const likeFrase = () => {
-    setLike(like + 1);
-    const likeFrase = frases[indice];
-    setCambio(likeFrase);
-  };
-
-  return (
-    <div>
-      <h1>Frases de Programación</h1>
-      <p>{frases[indice]}</p>
-      <button onClick={siguienteFrase}>Siguiente Frase</button>
-      <p>Frases cambiadas: {cambio}</p>
-      <p>Me gusta: {like}</p>
-      <button onClick={likeFrase}>Me gusta</button>
-    </div>
-  );
+  return <div></div>;
 };
 
 export default App;
--- src/Componentes/Login.jsx
+++
@@ -1,8 +0,0 @@
-export default function Login({ handleLogin }) {
-  return (
-    <>
-      <h1>El usuario es logeado o no</h1>
-      <p>{handleLogin ? "Esta logeado " : "No esta logeado"}</p>
-    </>
-  );
-}
