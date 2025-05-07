d3e21a40854f372a0f1cc777f9e42cf811337461
Abel Contreras <lucas_65_@hotmail.es>
2025-05-07 12:21:12 +0200

    Agrego Contador

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,16 @@
+import { useState } from "react";
+
 const App = () => {
-  return <div></div>;
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <h1>Contador: {count}</h1>
+      <button onClick={() => setCount(count + 1)}>Incrementar</button>
+      <button onClick={() => setCount(count - 1)}>Decrementar</button>
+      <button onClick={() => setCount(0)}>Reiniciar</button>
+    </div>
+  );
 };
 
 export default App;
